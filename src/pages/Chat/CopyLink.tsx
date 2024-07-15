0a0dcabafe65a2b1df197cf82d92c82010dfbaad
Adeyemi Abiade <abiadeabdulazeez@gmail.com>
2024-07-14 21:32:10 -0400

    Removed unused link import

--- src/pages/Chat/CopyLink.tsx
+++ src/pages/Chat/CopyLink.tsx
@@ -1,5 +1,5 @@
 import { BiClipboard } from "react-icons/bi";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const CopyLink = () => {
     const { joinID } = useParams();
